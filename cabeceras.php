c234c95865467d5f291f8469badf546ff45cbdaf
apuertas <tsm.antonia@gmail.com>
2021-06-23 19:36:41 +0200

    calse cabeceras

---
+++ cabeceras.php
@@ -0,0 +1,23 @@
+<?php
+class Cabeceras {
+  public static function getCabeceras(){
+    // apache_request_headers replicement for nginx
+    if (!function_exists('apache_request_headers')) {
+                foreach($_SERVER as $key=>$value) {
+                    if (substr($key,0,5)=="HTTP_") {
+                        $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5)))));
+                        $out[$key]=$value;
+                    }else{
+                        $out[$key]=$value;
+    		}
+                }
+                return $out;
+    }else{
+      return apache_request_headers();
+    }
+  } 
+
+}
+
+
+ ?>
